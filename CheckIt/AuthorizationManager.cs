e07a6d2f4c92c9bd949deab774e235f0d82b4d49
Alex Philayvanh <aphilay@gmail.com>
2018-12-10 20:59:05 -0800

    Add files via upload

---
+++ CheckIt/AuthorizationManager.cs
@@ -0,0 +1,58 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIt.Authorizations
+{
+    public class AuthorizationManager
+    {
+        /// <summary>
+        /// This method is to authorize a specific action 
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="actionRole"></param>
+        /// <returns></returns>
+        public static bool AuthorizeAction(IToken token, string action)
+        {
+            string client = token.GetClient();
+            if (client == null)
+            {
+                return Authorization.AuthorizeAction(token, action);
+            }
+            else
+            {
+                if(Authorization.CheckClientActions(token, action))
+                {
+                    return Authorization.AuthorizeAction(token, action);
+                }
+                return false;
+            }     
+            
+        }
+
+        public static bool AuthorizeUserToUser(IToken token, string email2, string action)
+        {
+            //checks if action string is NULL or empty
+            if (action.IsNullOrEmpty == true)
+            {
+                return false;
+            }
+            //checks if user can edit second user
+            if(!Authorization.UserToUserPermission(token,email2))
+            {
+                return false;
+            }
+
+            //checks if user has action
+            if(!Authorization.AuthorizeAction(token,action))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
